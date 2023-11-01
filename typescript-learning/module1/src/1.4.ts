ec2fd2244eba754f77fdcdd71cbe326b9ff96626
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-11-01 21:58:06 +0600

    Learned about data types in type screep

---
+++ typescript-learning/module1/src/1.4.ts
@@ -0,0 +1,24 @@
+//Basic Data Type
+
+//string
+let firstName : string = 'Rakib';
+//number
+let roll : number = 123;
+//boolean 
+let isAdmin : boolean = true;
+//undefine
+let x : undefined = undefined;
+//null
+let y : null = null;
+
+let d : number;
+d = 123;
+
+//Array
+let friends : string[] = ['Rachel','Monica'];
+let rollNumber : number[] = [1,2,3,4,5];
+
+//tuple --> array order --> type of values 
+let coOrdinate : [number,number] = [5,10];
+let ageName : [number, string, boolean] = [50,"Rakib",true];
+ageName[0] = 26;
