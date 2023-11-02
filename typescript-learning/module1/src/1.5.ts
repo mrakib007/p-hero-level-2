83b741e7bdb6379408dcb4cdc8959b48fd9d1486
mrakib007 <mrakibhasan199744@gmail.com>
2023-11-02 12:05:56 +0600

    object, optional and literal

---
+++ typescript-learning/module1/src/1.5.ts
@@ -0,0 +1,19 @@
+//reference type --> object
+
+const user : {
+    // company: string;
+    readonly company: string;
+    // company: 'BacBon'; //type --> literal type this value will always be fixed.
+    firstName: string;
+    middleName? : string; //optional type
+    lastName: string;
+    isMarried: boolean;
+} = {
+    company: 'BacBon',
+    firstName : 'Rakib',
+    // middleName: 'Hasan',
+    lastName: 'Hasan',
+    isMarried: false,
+}
+
+user.
