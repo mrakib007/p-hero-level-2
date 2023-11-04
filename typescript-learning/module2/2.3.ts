48612bee7400fed0b8bce6960ea499b6250868b5
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-11-04 17:21:47 +0600

    Introduction to generics

---
+++ typescript-learning/module2/2.3.ts
@@ -0,0 +1,39 @@
+{
+    //generic type
+
+    // type GenericArray = Array<number>
+    type GenericArray<T> = Array<T>
+
+    // const rollNumbers : number[] = [2,6,5];
+    // const rollNumbers : Array<number> = [2,6,5];
+    const rollNumbers : GenericArray<number> = [2,6,5];
+
+    // const mentors : string[] = ['Rakib','Sakib','Yeamin'];
+    // const mentors : Array<string> = ['Rakib','Sakib','Yeamin'];
+    const mentors : GenericArray<string> = ['Rakib','Sakib','Yeamin'];
+
+    // const boolArray : boolean[] = [true,false,true];
+    // const boolArray : Array<boolean> = [true,false,true];
+    const boolArray : GenericArray<boolean> = [true,false,true];
+
+    const user : GenericArray<{name: string,age: number}> = [
+        {
+            name: 'Rakib',
+            age: 26,
+        },
+        {
+            name: 'Sakib',
+            // age: "18",
+            age: 18,
+        },
+    ]
+
+    const add = (x : number, y : number) => x + y;
+
+    add(10,20);
+
+    //generic tuple
+    type GenericTuple<x,y> = [x,y]
+    const human : GenericTuple<string,string> = ['Rakib','Humaira'];
+    const userWitId : GenericTuple<number,{name: string,email: string}> = [12,{name: 'Rakib',email: 'rakib@gmail.com'}];
+}
