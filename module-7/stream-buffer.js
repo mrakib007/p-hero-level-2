1079c5d82b17e0fcf829ae62dc8646643e2c082a
mrakib007 <mrakibhasan199744@gmail.com>
2023-11-20 14:36:57 +0600

    Stream and buffer

---
+++ module-7/stream-buffer.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const fs = require('fs');
+//creating a server using raw node js
+const server = http.createServer();
+
+//listener
+server.on('request', (req, res) => {
+    if (req.url === '/read-file' && req.method === 'GET');
+
+    //streaming file reading
+    const readableStream = fs.createReadStream(process.cwd() + '/texts/read.txt');
+    readableStream.on('data', (buffer) => {
+        res.write(buffer)
+    })
+    readableStream.on('end', () => {
+        res.end('Hello from server');
+    })
+})
+server.listen(5000, () => {
+    console.log('server is listening on port 5000');
+})
