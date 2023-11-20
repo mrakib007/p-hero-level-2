d27adbb6e9aeb310316bd464c4a3fbf2012f11b0
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-11-20 20:11:06 +0600

    changes

--- module-7/stream-buffer.js
+++ module-7/stream-buffer.js
@@ -10,10 +10,18 @@ server.on('request', (req, res) => {
     //streaming file reading
     const readableStream = fs.createReadStream(process.cwd() + '/texts/read.txt');
     readableStream.on('data', (buffer) => {
+        res.statusCode = 200;
         res.write(buffer)
     })
     readableStream.on('end', () => {
-        res.end('Hello from server');
+        res.statusCode = 200;
+        res.end('The streaming is over');
+    })
+
+    readableStream.on('error', (error) => {
+        console.log(error);
+        res.statusCode = 500;
+        res.end('Something went wrong!');
     })
 })
 server.listen(5000, () => {
