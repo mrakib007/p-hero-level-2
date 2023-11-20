db4d5bc869f40c5c4807b41f0b212df36b58e285
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-11-20 22:12:47 +0600

    Learning about routing in express.js

--- module-7/project-1/dist/app.js
+++ module-7/project-1/dist/app.js
@@ -9,13 +9,42 @@ const port = 3000;
 //parsers
 app.use(express_1.default.json());
 app.use(express_1.default.text());
-app.get("/", (req, res) => {
+const userRouter = express_1.default.Router();
+const courseRouter = express_1.default.Router();
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/courses", courseRouter);
+userRouter.post("/create-user", (req, res) => {
+    const user = req.body;
+    console.log(user);
+    res.json({
+        success: true,
+        message: "User is created successfully",
+        data: user,
+    });
+});
+courseRouter.post("/create-course", (req, res) => {
+    const course = req.body;
+    console.log(course);
+    res.json({
+        success: true,
+        message: "User is created successfully",
+        data: course,
+    });
+});
+const logger = (req, res, next) => {
+    console.log(req.url, req.method, req.hostname);
+    next();
+};
+// app.get("/:userId/:subid",(req : Request,res : Response)=>{ //params
+app.get("/", logger, (req, res) => {
+    // console.log(req.params);
+    // console.log(req.query);
     res.send("Hello developers,Good morning");
 });
-app.post('/', (req, res) => {
+app.post("/", logger, (req, res) => {
     console.log(req.body);
     res.json({
-        message: "Successfully received data"
+        message: "Successfully received data",
     });
 });
 exports.default = app;
--- module-7/project-1/src/app.ts
+++ module-7/project-1/src/app.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from "express";
 const app = express();
 const port = 3000;
 
@@ -6,13 +6,48 @@ const port = 3000;
 app.use(express.json());
 app.use(express.text());
 
-app.get("/",(req : Request,res : Response)=>{
-    res.send("Hello developers,Good morning");
+const userRouter = express.Router();
+const courseRouter = express.Router();
+
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/courses", courseRouter);
+
+userRouter.post("/create-user", (req: Request, res: Response) => {
+  const user = req.body;
+  console.log(user);
+
+  res.json({
+    success: true,
+    message: "User is created successfully",
+    data: user,
+  });
 });
-app.post('/',(req : Request,res : Response)=>{
-    console.log(req.body);
-    res.json({
-        message: "Successfully received data"
-    });
-})
+
+courseRouter.post("/create-course", (req: Request, res: Response) => {
+  const course = req.body;
+  console.log(course);
+  res.json({
+    success: true,
+    message: "User is created successfully",
+    data: course,
+  });
+});
+
+const logger = (req: Request, res: Response, next: NextFunction) => {
+  console.log(req.url, req.method, req.hostname);
+  next();
+};
+
+// app.get("/:userId/:subid",(req : Request,res : Response)=>{ //params
+app.get("/", logger, (req: Request, res: Response) => {
+  // console.log(req.params);
+  // console.log(req.query);
+  res.send("Hello developers,Good morning");
+});
+app.post("/", logger, (req: Request, res: Response) => {
+  console.log(req.body);
+  res.json({
+    message: "Successfully received data",
+  });
+});
 export default app;
