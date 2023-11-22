3df4bcbe6815c4a5ea1af3100c2d19438b15067c
mrakib007 <mrakibhasan199744@gmail.com>
2023-11-22 16:02:24 +0600

    learing mongoose

--- module-8/first-project/src/server.ts
+++ module-8/first-project/src/server.ts
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 async function main(){
-    await mongoose.connect('mongodb://120.0.0.1:27017/test');
+    await mongoose.connect(process.env.DATABASE_URL);
 }
 
 app.listen(port, () => {
