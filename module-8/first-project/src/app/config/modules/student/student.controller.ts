ccf5c42331de166bd9b76f7e0b6b0f3645d1dbe9
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-11-25 14:22:13 +0600

    video 8-10 4:54 minutes

--- module-8/first-project/src/app/config/modules/student/student.controller.ts
+++ module-8/first-project/src/app/config/modules/student/student.controller.ts
@@ -3,7 +3,7 @@ import { StudentServices } from './student.service';
 
 const createStudent = async (req: Request, res: Response) => {
   try {
-    const {student : studentData} = req.body;
+    const { student: studentData } = req.body;
     //will call service function to send this data
     const result = await StudentServices.createStudentIntoDB(studentData);
     //send response
@@ -17,6 +17,20 @@ const createStudent = async (req: Request, res: Response) => {
   }
 };
 
+const getAllStudents = async (req: Request, res: Response) => {
+  try {
+    const result = await StudentServices.getAllStudentsFromDb();
+    res.status(200).json({
+      success: true,
+      message: 'Students are retrieved successfully',
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 export const StudentController = {
-    createStudent,
-}
+  createStudent,
+  getAllStudents,
+};
--- module-8/first-project/src/app/config/modules/student/student.service.ts
+++ module-8/first-project/src/app/config/modules/student/student.service.ts
@@ -5,7 +5,12 @@ const createStudentIntoDB = async (student: Student) => {
   const result = await StudentModel.create(student);
   return result;
 };
+const getAllStudentsFromDb = async () => {
+  const result = await StudentModel.find();
+  return result;
+};
 
 export const StudentServices = {
   createStudentIntoDB,
+  getAllStudentsFromDb,,
 };
